c4be8cba988d2e5a82237399c423bcee2f3eccb5
sebikolon <opensource@sbuechler.de>
2020-03-16 16:56:15 +0100

    Fixed + updated scripts

--- openSSL_skripts/001_createRootCA.bat
+++ openSSL_skripts/001_createRootCA.bat
@@ -5,7 +5,7 @@
 ::		https://sbuechler.de
 ::		https://github.com/sebikolon/PKI-X509
 ::
-::		Last Release: 06 March 2020	
+::		Last Release: 16 March 2020	
 ::
 :: ***************************************************************************************
 ::
@@ -28,13 +28,20 @@
 	ECHO # Creating ROOT CA and according files ..
 	
 	if exist openssl.cfg (
-	
+		
+		REM if exist private/ca.key.pem (
+		REM 	SET /P _OVERWRITECAKEY Do you really want to overwrite the existing private key of your ROOT CA [Y/N]?
+		REM 	if %_OVERWRITECAKEY% NEQ N (
+		REM 		:: Go back to original directory
+		REM 		cd %_ORIGINDIR% 
+		REM 		exit /b 0
+		REM )
 		openssl genrsa -aes256 -out private\ca.key.pem 4096	
 		openssl req -config openssl.cfg  -key private\ca.key.pem  -new -x509 -days 7300 -sha256 -extensions v3_ca  -out certs\ca.cert.pem 
 	
 		ECHO.
 		ECHO    .. OK!
-		ECHO # In order to proceed with the creation of the intermediate CA, please run next script
+		ECHO # In order to proceed with the creation of the intermediate CA, please run next script!
 		ECHO.
 	) else (
 		ECHO    .. Error! File '%_BASISPFAD%\openssl.cfg' does not exist or is missing valid paths!
--- openSSL_skripts/002_prepareIntermediateCA.bat
+++ openSSL_skripts/002_prepareIntermediateCA.bat
@@ -1,22 +1,85 @@
+::
+:: ***************************************************************************************
+::
+::		sebikolon PKI-X509
+::		https://sbuechler.de
+::		https://github.com/sebikolon/PKI-X509
+::
+::		Last Release: 16 March 2020	
+::
+:: ***************************************************************************************
+::
+
 @ECHO OFF
-SET _ORIGINDIR=%cd%
 
-SET _INTER=intermediate
-SET _INTERCONFIG=openssl_inter.cfg
+		
+	ECHO PKI-X509 - Prepare Certification Authority (CA)
+	ECHO Copyright MIT
+	ECHO https://sbuechler.de
+	ECHO.
+	
+	SET _ORIGINDIR=%cd%
+    SET _INTER=intermediate
+    SET _INTERCONFIG=openssl_inter.cfg
+	ECHO # Please choose the base directory you defined before (e.g. 'C:\myPKI').
+	SET /P _BASISPFAD= Type, then press ENTER:
 
-SET /P _BASISPFAD= Geben Sie das zuvor angelegte Basisverzeichnis an (Z.B. C:\test):
+    ::  Create an intermediate CA ::
+    ECHO.
+	ECHO # Creating INTERMEDIATE CA and according files ..    
+    mkdir %_BASISPFAD%\%_INTER%
+    cd %_BASISPFAD%\%_INTER%
+    
+    if exist certs (
+		echo    '%_BASISPFAD%\%_INTER%\certs' already exists. Skipping .. 
+	) else (
+		mkdir certs
+	)
+	
+	if exist crl (
+		echo    '%_BASISPFAD%\%_INTER%\crl' already exists. Skipping .. 
+	) else (
+		mkdir crl
+	)
+	
+	if exist newcerts (
+		echo    '%_BASISPFAD%\%_INTER%\newcerts' already exists. Skipping .. 
+	) else (
+		mkdir newcerts
+	)
+	
+	if exist private (
+		echo    '%_BASISPFAD%\%_INTER%\private' already exists. Skipping .. 
+	) else (
+		mkdir private
+	)
+	
+	if exist csr (
+		echo    '%_BASISPFAD%\%_INTER%\csr' already exists. Skipping .. 
+	) else (
+		mkdir csr
+	)
 
-::  Erstellen einer Intermediate-CA ::
-mkdir %_BASISPFAD%\%_INTER%
-cd %_BASISPFAD%\%_INTER%
+	:: Container that holds PKI structure
+	if exist index.txt (
+		echo    '%_BASISPFAD%\%_INTER%\index.txt' already exists. Skipping .. 
+	) else (
+		type NUL > index.txt
+	)
+	
+	:: File that holds latest serial number
+	if exist serial (
+		echo    '%_BASISPFAD%\%_INTER%\serial' already exists. Skipping .. 
+	) else (
+		echo 01 > serial
+	)    
 
-mkdir certs crl newcerts private csr
-type NUL > index.txt
-echo 01 > serial
+    :: Go back to original directory
+	cd %_ORIGINDIR%
 
-:: Wechsle zurück zum Ursprungsverzeichnis
-cd %_ORIGINDIR%
-
-echo Erfolg! Kopieren Sie nun bitte die Datei '%_INTERCONFIG%' in den Ordner '%_BASISPFAD%\%_INTER%' und passen Sie die Pfade darin an!
+	ECHO    .. OK!
+	ECHO.
+	ECHO # In order to proceed, please copy the file ''%_INTERCONFIG%' into folder '%_BASISPFAD%\%_INTER%' and adjust the paths within this file!
+	ECHO.    
 
 pause
